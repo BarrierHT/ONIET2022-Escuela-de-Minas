a28561532ab805f29ab38d2100ecb7e80b82eddb
fervill <marvillisi@gmail.com>
2022-10-27 11:21:46 -0300

    Base commit

---
+++ oniet/src/main/java/desarrollo/oniet/OnietApplication.java
@@ -0,0 +1,13 @@
+package desarrollo.oniet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnietApplication.class, args);
+	}
+
+}
---
+++ oniet/src/test/java/desarrollo/oniet/OnietApplicationTests.java
@@ -0,0 +1,13 @@
+package desarrollo.oniet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
